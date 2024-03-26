f34d5c8ba305f86fd042a834ce0639d53735dabf
Hafsal <hafsal@Hafsals-MacBook-Pro.local>
2024-03-26 17:55:22 +0530

    image banner

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -4,37 +4,19 @@ export default function decorate(block) {
     [...block.children].forEach((row, rowNo) => {
         
         if (rowNo === 0) {
-            row.classList.add('banner-homepage');
-            row.classList.add('homepg');  
+            row.classList.add('image-banner-main');
         }
         [...row.children].forEach((col, colNo) => {
-
-            if (colNo === 0) {
-                col.classList.add('banner-text');
-            }
-
-            const pic = col.querySelector('picture');
-            if (pic) {
-                const picWrapper = pic.closest('div');
-                if (picWrapper) {
-                    picWrapper.classList.add('banner-image');
+         
+                let image= col.querySelector('picture');
+                
+                if(image) {
+                
+                    col.classList.add('image-banner-image');    
+                } else {
+                    
+                    col.classList.add('image-banner-text');
                 }
-            }
-            
-            const button = col.querySelector('a.button');
-            if (button) {
-                button.classList.add('how-it-work-button');     
-                button.classList.add('webbutton');
-                
-                let buttonParent = button.parentNode.parentNode;
-                buttonParent.replaceWith(button);
-                
-                let button2 = button.cloneNode(true);
-                button2.classList.remove('webbutton');
-                button2.classList.add('mobbutton');
-                let parent2 = button.parentNode.parentNode;               
-                parent2.append(button2);  
-            }
         });
     });
 }
